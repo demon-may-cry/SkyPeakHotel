f82a5d4103be62e4dc420bcce90ae40630e31cd0
Дмитрий Ельцов <dmcswg@gmail.com>
2026-01-14 23:20:32 +0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/skypeak/hotel/SkyPeakHotelApplication.java
@@ -0,0 +1,13 @@
+package com.skypeak.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyPeakHotelApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SkyPeakHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skypeak/hotel/SkypeakHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypeak.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkypeakHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
