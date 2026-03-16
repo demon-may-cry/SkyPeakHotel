5e3841d801e4e17e5f397943414d1eeb03557d6f
Дмитрий Ельцов <dmcswg@gmail.com>
2026-03-16 23:12:43 +0300

    feat: add logging

--- src/main/java/com/skypeak/hotel/service/impl/RegistrationServiceImpl.java
+++ src/main/java/com/skypeak/hotel/service/impl/RegistrationServiceImpl.java
@@ -9,6 +9,7 @@ import com.skypeak.hotel.repository.UserBalanceRepository;
 import com.skypeak.hotel.repository.UserRepository;
 import com.skypeak.hotel.service.RegistrationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +23,7 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 @Service
 @Transactional
+@Slf4j
 public class RegistrationServiceImpl implements RegistrationService {
 
     private final UserRepository userRepository;
@@ -31,19 +33,28 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public void register(RegisterRequest request) {
+        log.info("Registering new user with email: {}", request.getEmail());
+
         if (userRepository.existsByEmail(request.getEmail()))
             throw new IllegalStateException("User with this email already exists");
+        log.info("Email {} is available for registration", request.getEmail());
 
         var role = roleRepository.findByName("USER").orElseThrow(() ->
                 new IllegalStateException("Role USER not found"));
+        log.warn("Role USER found for registration");
 
         var user = new UserEntity();
         user.setEmail(request.getEmail());
         user.setPassword(passwordEncoder.encode(request.getPassword()));
         user.setRole(role);
         user.setStatus(Status.ACTIVE);
+        log.info("Creating user entity: {}, {}, {}",
+                user.getEmail(),
+                user.getRole().getName(),
+                user.getStatus());
 
         userRepository.save(user);
+        log.info("User {} registered successfully", user.getEmail());
 
         var balance = new UserBalanceEntity();
         balance.setUser(user);
@@ -51,5 +62,8 @@ public class RegistrationServiceImpl implements RegistrationService {
         balance.setUpdatedAt(LocalDateTime.now());
 
         balanceRepository.save(balance);
+        log.info("Initial balance created for user {} with amount {}",
+                user.getEmail(),
+                balance.getBalance());
     }
 }
