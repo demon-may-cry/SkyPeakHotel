4baaf4fc1f8c7bf0d6a0b34cfc3bbb0e50735ee3
Дмитрий Ельцов <dmcswg@gmail.com>
2026-03-17 00:43:37 +0300

    feat: add exception invalid format

--- src/main/java/com/skypeak/hotel/exception/GlobalExceptionHandler.java
+++ src/main/java/com/skypeak/hotel/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -105,6 +106,17 @@ public class GlobalExceptionHandler {
                 ERROR_BAD_REQUEST,
                 request.getRequestURI()
         );
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleInvalidFormat(
+            HttpMessageNotReadableException ex,
+            HttpServletRequest request) {
+        log.error("Invalid request format: {}", ex.getMessage());
+        return buildError(
+                HttpStatus.BAD_REQUEST,
+                ERROR_BAD_REQUEST,
+                request.getRequestURI());
     }
 
     @ExceptionHandler(Exception.class)
